fc517fc990db2b7461c451ccf67c225ba8a96025
Mark Skelton <mdskelton99@gmail.com>
2025-04-18 08:33:40 -0500

    Sync

--- web/app/Progress.tsx
+++ web/app/Progress.tsx
@@ -6,7 +6,7 @@ export function Progress({
 	passages: Passage[]
 }) {
 	return (
-		<div className="flex gap-2 w-full py-4 px-6 sticky top-0 dark:text-gray-50 dark:bg-gray-950 z-10">
+		<div className="flex gap-2 w-full py-2 px-6 sticky top-0 mt-6 bg-white dark:text-gray-50 dark:bg-gray-950 z-10">
 			{passages.map((passage) => (
 				<ProgresPill key={passage.ref.ref} passage={passage} />
 			))}
@@ -20,8 +20,25 @@ function ProgresPill({
 	passage: Passage
 }) {
 	return (
-		<div className="rounded-full bg-gray-800 px-4 text-xs py-1 w-full text-center font-semibold">
-			{passage.bookTitle} {passage.ref.chapter}
+		<div className="rounded-full bg-gray-200 dark:bg-gray-800 px-4 text-xs py-1 w-full text-center font-semibold relative before:absolute before:left-0 before:top-0 before:bottom-0 before:bg-blue-500 before:w-0 before:rounded-full before:transition-[width]">
+			<div className="relative">
+				{passage.bookTitle} {passage.ref.chapter}
+			</div>
 		</div>
 	)
 }
+// .tracked-element {
+//   view-timeline-name: --progress;
+//   view-timeline-axis: block;
+// }
+//
+// .animated-element {
+//   animation: fadeIn linear;
+//   animation-timeline: --progress;
+//   animation-range: entry 0% to exit 100%;
+// }
+//
+// @keyframes fadeIn {
+//   from { opacity: 0; }
+//   to { opacity: 1; }
+// }
