d29eccdc807226ead9b7da951742cd8a9ffb1c30
Mark Skelton <mdskelton99@gmail.com>
2025-05-01 21:35:11 -0500

    Add download

---
+++ web/app/api/download/[translation]/route.ts
@@ -0,0 +1,53 @@
+import { bible, sql } from '@/app/lib/db'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(
+	_request: Request,
+	{ params }: { params: Promise<{ translation: string }> },
+) {
+	const { translation } = await params
+
+	const encoder = new TextEncoder()
+	const stream = new ReadableStream({
+		start(controller) {
+			const writeRows = (type: string, stmt: string, param: string) => {
+				const iter = bible.prepare<any, any>(stmt).iterate(param)
+
+				for (const row of iter) {
+					const data = Object.values(row)
+
+					controller.enqueue(
+						encoder.encode(`${JSON.stringify([type, ...data])}\n`),
+					)
+				}
+			}
+
+			writeRows(
+				'book',
+				sql`SELECT ref, title FROM book WHERE translation_ref = ?`,
+				translation,
+			)
+
+			writeRows(
+				'chapter',
+				sql`SELECT ref, data FROM chapter WHERE ref LIKE ?`,
+				`%.${translation}`,
+			)
+
+			writeRows(
+				'range',
+				sql`SELECT start, end, word_count FROM range WHERE chapter_ref = ?`,
+				`%.${translation}`,
+			)
+
+			controller.close()
+		},
+	})
+
+	return new Response(stream, {
+		headers: {
+			'Content-Type': 'application/x-ndjson',
+		},
+	})
+}
