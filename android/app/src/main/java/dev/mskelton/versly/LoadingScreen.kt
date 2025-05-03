a9113eea63fc62e2b9431ed0d708a679b5814cd6
Mark Skelton <mdskelton99@gmail.com>
2025-05-02 21:48:33 -0500

    Loading

--- android/app/src/main/java/dev/mskelton/versly/LoadingScreen.kt
+++ android/app/src/main/java/dev/mskelton/versly/LoadingScreen.kt
@@ -1,5 +1,6 @@
 package dev.mskelton.versly
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,12 +15,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingScreen(translation: String) {
     Box(modifier = Modifier.fillMaxSize()) {
+        val image = painterResource(id = R.drawable.ic_launcher_foreground)
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -27,18 +33,34 @@ fun LoadingScreen(translation: String) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = image,
+                contentDescription = stringResource(R.string.versly_logo),
+            )
             Text(
-                text = "Downloading $translation Bible",
+                text = stringResource(R.string.downloading_bible, translation),
                 style = MaterialTheme.typography.titleLarge,
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(24.dp))
-            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp, 0.dp)
+            )
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = "This should only take a few seconds",
+                text = stringResource(R.string.this_should_only_take_a_few_seconds),
                 style = MaterialTheme.typography.bodyMedium
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun LoadingScreenPreview() {
+    PreviewScaffold {
+        LoadingScreen(translation = "NIV")
     }
 }
