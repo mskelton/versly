cf28ca3a1decfec4e0028af961cc35e50dbe652d
Mark Skelton <mdskelton99@gmail.com>
2025-05-03 23:46:37 -0500

    Update theme

--- android/app/src/main/java/dev/mskelton/versly/ui/theme/Type.kt
+++ android/app/src/main/java/dev/mskelton/versly/ui/theme/Type.kt
@@ -1,34 +1,59 @@
 package dev.mskelton.versly.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import dev.mskelton.versly.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+@OptIn(ExperimentalTextApi::class)
+val fontFamily = FontFamily(
+    Font(
+        R.font.rubik,
+        weight = FontWeight.Normal,
+        variationSettings = FontVariation.Settings(FontVariation.weight(400)),
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    Font(
+        R.font.rubik,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(FontVariation.italic(1.0f)),
+    ),
+    Font(
+        R.font.rubik,
+        weight = FontWeight.Bold,
+        variationSettings = FontVariation.Settings(FontVariation.weight(700)),
+    ),
+    Font(
+        R.font.rubik,
+        weight = FontWeight.Bold,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            FontVariation.weight(700),
+            FontVariation.italic(1.0f),
+        ),
+    ),
+)
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val Typography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = fontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = fontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = fontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = fontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = fontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = fontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = fontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = fontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = fontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = fontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = fontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = fontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = fontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = fontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = fontFamily),
 )
