102f5a55a33f5d0deba1de59c5093bd16f85da43
Mark Skelton <mdskelton99@gmail.com>
2025-11-29 23:47:48 -0600

    Icons

--- ios/Versly/Versly/ContentView.swift
+++ ios/Versly/Versly/ContentView.swift
@@ -3,19 +3,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            Tab("Read", systemImage: "tray.and.arrow.down.fill") {
+            Tab("Read", systemImage: "book.fill") {
                 ReadView()
             }
 
-            Tab("Plans", systemImage: "tray.and.arrow.up.fill") {
+            Tab("Plans", systemImage: "checklist") {
                 PlansView()
             }
 
-            Tab("Search", systemImage: "person.crop.circle.fill") {
+            Tab("Search", systemImage: "magnifyingglass") {
                 SearchView()
             }
 
-            Tab("Settings", systemImage: "person.crop.circle.fill") {
+            Tab("Settings", systemImage: "gear") {
                 SettingsView()
             }
         }
