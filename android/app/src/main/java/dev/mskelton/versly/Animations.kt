2b394ea7fa6e53222bc68bb3fecb743b95b77109
Mark Skelton <mdskelton99@gmail.com>
2026-02-24 23:17:45 -0600

    Animation helpers

---
+++ android/app/src/main/java/dev/mskelton/versly/Animations.kt
@@ -0,0 +1,34 @@
+package dev.mskelton.versly
+
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.ui.unit.IntOffset
+
+private const val DURATION = 200
+private const val OFFSET_FRACTION = 0.25f
+
+fun horizontalSlideTransition(direction: Int): ContentTransform {
+    val spec = tween<IntOffset>(durationMillis = DURATION, easing = FastOutSlowInEasing)
+    val fadeSpec = tween<Float>(durationMillis = DURATION, easing = FastOutSlowInEasing)
+
+    return slideInHorizontally(spec, initialOffsetX = { (it * OFFSET_FRACTION * direction).toInt() }) +
+        fadeIn(fadeSpec) togetherWith
+        slideOutHorizontally(spec, targetOffsetX = { (-it * OFFSET_FRACTION * direction).toInt() }) +
+        fadeOut(fadeSpec)
+}
+
+fun horizontalSlidePopTransition(direction: Int): ContentTransform {
+    val spec = tween<IntOffset>(durationMillis = DURATION, easing = FastOutSlowInEasing)
+    val fadeSpec = tween<Float>(durationMillis = DURATION, easing = FastOutSlowInEasing)
+
+    return slideInHorizontally(spec, initialOffsetX = { (-it * OFFSET_FRACTION * direction).toInt() }) +
+        fadeIn(fadeSpec) togetherWith
+        slideOutHorizontally(spec, targetOffsetX = { (it * OFFSET_FRACTION * direction).toInt() }) +
+        fadeOut(fadeSpec)
+}
